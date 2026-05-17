8e09f1786c2b1adce6e623a81b4575619ce56cc2
yani-again <historicalsection83@proton.me>
2026-05-17 12:29:12 +0100

    worked on projects page

--- code/index.js
+++ code/index.js
@@ -20,3 +20,32 @@ setTimeout(() => {
 setTimeout(() => {
     loading_wrapper.style.display = "none";
 }, (Number(total_loading_delay) + Number(extra_loading_delay) + 400) * ANIMATIONS_ON);
+
+
+const nav_items = document.querySelectorAll(".nav-item");
+
+function change_page(to_page)
+{
+    nav_items.forEach(item => {
+        if (item.dataset.active != 1)
+            return;
+        if (item.textContent.toLowerCase() == to_page)
+            return;
+
+        let current_nav_item = item;
+        let new_nav_item = document.querySelector("#nav-" + to_page);
+        let current_page = document.querySelector("#" + item.textContent.toLowerCase());
+        let new_page = document.querySelector("#" + to_page);
+
+        current_nav_item.dataset.active = 0;
+        current_nav_item.classList.remove("active");
+        current_nav_item.classList.add("inactive");
+
+        new_nav_item.dataset.active = 1;
+        new_nav_item.classList.remove("inactive");
+        new_nav_item.classList.add("active");
+
+        current_page.classList.toggle("page-inactive");
+        new_page.classList.toggle("page-inactive");
+    })
+}
